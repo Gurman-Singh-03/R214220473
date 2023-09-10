8d980ba68eb56b0a3f2a7f4db2fe428847dcb12d
Gurman-Singh-03 <128717943+Gurman-Singh-03@users.noreply.github.com>
2023-09-10 11:35:33 +0530

    Update main.py

--- Question-1/main.py
+++ Question-1/main.py
@@ -1,30 +1,29 @@
-def get_numbers(urls):
+import requests
+import json
+import logging
+
+def get_numbers(urls, timeout=5):
   """Gets the numbers from the specified URLs."""
-  # This function takes a list of URLs as input and returns a list of numbers.
-  # The function makes a request to each URL and parses the response as JSON.
+  # This function takes a list of URLs as input and returns a list of numbers,makes a request to each URL and parses the response as JSON.
   # The numbers in the JSON response are then added to the list of numbers.
+  # The timeout parameter specifies the maximum amount of time to wait for each request.
 
   numbers = []
   for url in urls:
-    # This loop iterates over the list of URLs.
-    response = requests.get(url)
-    # This makes a request to the specified URL.
+    response = requests.get(url, timeout=timeout)
     if response.status_code == 200:
-      # This checks if the response status code is 200, which means that the request was successful.
       data = json.loads(response.content)
-      # This parses the response as JSON.
       numbers.extend(data["numbers"])
-    # This adds the numbers in the JSON response to the list of numbers.
+
   return numbers
 
 def main():
-  """The main function."""
-  # This is the main function.
   urls = ["http://20.244.56.144/numbers/primes", "http://20.244.56.144/numbers/fibo"]
   # This defines a list of URLs.
-  numbers = get_numbers(urls)
+  logging.basicConfig(level=logging.INFO)
+  numbers = get_numbers(urls, timeout=5)
   # This calls the `get_numbers()` function and stores the results in the `numbers` variable.
-  print(json.dumps({"numbers": numbers}))
+  logging.info("The numbers are: %s", numbers)
   # This prints the results to the terminal.
 
 if _name_ == "_main_":
